e9a400e465de77e0fb804e01c5588ec4a305f622
Wael Emara <wael.s.emara@gmail.com>
2013-12-09 16:15:56 +1100

    Add the ui for new post with the rich control still the add function does not work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
+  layout 'application_admin', :only => [:new]
+  
   def index
     @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
   end
 end
 
