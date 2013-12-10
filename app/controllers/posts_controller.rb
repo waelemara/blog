c994af4ab3f0c46a23dbf94c8b084915e87a7a34
Wael Emara <wael.s.emara@gmail.com>
2013-12-10 12:57:11 +1100

    Add basic save for posts and very basic show for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   layout 'application_admin', :only => [:new]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
     @posts = Post.all
@@ -8,5 +9,29 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
+  def create    
+    @post = Post.new(post_params)
+    respond_to do |format|
+         if @post.save
+           format.html { redirect_to @post, notice: 'Post was successfully created.' }
+           format.json { render action: 'show', status: :created, location: @post }
+         else
+           format.html { render action: 'new' }
+           format.json { render json: @post.errors, status: :unprocessable_entity }
+         end
+    end
+  end
+  
+  
+  def show
+  end
+    
+  private
+    def set_post
+        @post = Post.find(params[:id])
+    end
+    def post_params
+          params.require(:post).permit(:title, :description , :body)
+    end
 end
-
